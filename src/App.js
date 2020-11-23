2cda5db73b4ebcca9654d4ec9f818583013c2569
PERFICIENT\sagar.mahalle <sagar.mahalle@perficient.com>
2020-11-23 12:09:15 +0530

    asas

---
+++ src/App.js
@@ -0,0 +1,58 @@
+import React, { useState, Component } from 'react';
+import logo from './logo.svg';
+import './App.css';
+import axios from 'axios';
+
+function App() {
+  const [books, setBooks] = useState();
+
+  const fetchData = async () => {
+    const response = await axios.get(
+      'https://www.anapioficeandfire.com/api/books?pageSize=30'
+    );
+
+    console.log(response.data);
+    setBooks(response.data);
+  };
+
+  return (
+    <div className="App">
+      <h1>Game of Thrones Books</h1>
+      <h2>Fetch a list from an API and display it</h2>
+
+      {/* Fetch data from API */}
+      <div>
+        <button className="fetch-button" onClick={fetchData}>
+          Fetch Data
+        </button>
+        <br />
+      </div>
+
+      {/* Display data from API */}
+      <div className="books">
+        {books &&
+          books.map((book, index) => {
+            const cleanedDate = new Date(book.released).toDateString();
+            const authors = book.authors.join(', ');
+
+            return (
+              <div className="book" key={index}>
+                <h3>Book {index + 1}</h3>
+                <h2>{book.name}</h2>
+
+                <div className="details">
+                  <p>👨: {authors}</p>
+                  <p>📖: {book.numberOfPages} pages</p>
+                  <p>🏘️: {book.country}</p>
+                  <p>⏰: {cleanedDate}</p>
+                </div>
+              </div>
+            );
+          })}
+      </div>
+
+    </div>
+  );
+}
+
+export default App;
